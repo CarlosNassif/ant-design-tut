370b787afe479ec91cb9c3c7153e712a5c071dde
Carlos Nassif <costa.carlos12@gmail.com>
2023-03-21 21:23:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import 'antd/dist/reset.css';
+import './App.css';
+
+import React, { useState } from 'react';
+
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={() => setCollapsed(!collapsed)}
+      >
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+          <Menu.Item key='1' icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key='2' icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
+            <Menu.Item key='3'>Tom</Menu.Item>
+            <Menu.Item key='4'>Bill</Menu.Item>
+            <Menu.Item key='5'>Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub2' icon={<UserOutlined />} title='User'>
+            <Menu.Item key='6'>Team 1</Menu.Item>
+            <Menu.Item key='7'>Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key='9' icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className='site-layout'>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default App;
